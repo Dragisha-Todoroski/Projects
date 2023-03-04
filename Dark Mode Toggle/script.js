5b5ba4d973987a0cf707a280ed9764e5db4d9a13
Dragisha-Todoroski <120037342+Dragisha-Todoroski@users.noreply.github.com>
2023-03-04 21:15:37 +0100

    Delete Dark Mode Toggle directory

--- Dark Mode Toggle/script.js
+++
@@ -1,8 +0,0 @@
-const darkModeBtn = document.querySelector('#darkModeBtn');
-
-darkModeBtn.addEventListener('click', darkLightModeSwitch);
-
-function darkLightModeSwitch() {
-    document.body.classList.toggle('darkMode');
-    darkModeBtn.classList.toggle('lightMode');
-}
